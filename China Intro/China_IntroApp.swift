a69b150abfcc0b3c9eef1f95cd0ea53d9ba06456
Bosco Choi <bchoi@lcs.on.ca>
2020-09-29 14:41:16 -0400

    Initial Commit

---
+++ China Intro/China_IntroApp.swift
@@ -0,0 +1,17 @@
+//
+//  China_IntroApp.swift
+//  China Intro
+//
+//  Created by Chun Choi on 2020-09-29.
+//
+
+import SwiftUI
+
+@main
+struct China_IntroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
